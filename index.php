64fa3e985550ccbb91b8057004d87acce22b1caa
NicholasTan24 <nicholastan_2327240005@mhs.mdp.ac.id>
2025-04-24 12:24:22 +0700

    mengubah php menjadi bootstrap

--- index.php
+++ index.php
@@ -3,26 +3,46 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Form Pendaftaran UKM</title>
+    <!-- Latest compiled and minified CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-    <h1>Form Pendaftaran UKM</h1>
-    <form action="proses_daftar.php" method="POST">
-        <input type="text" name="var_npm" placeholder = "Input NPM">
-        <br/><br/>
-        <input type="text", name="var_nama" placeholder = "Input Nama">
-        <br/><br/>
-        Pilihan UKM :
-        <select name="var_ukm">
-            <option>UKM Programming</option>
-            <option>UKM Band</option>
-            <option>UKM Futsal</option>
-            <option>UKM Basket</option>
-        </select>
-        <br/><br/>
-        <input type="submit" value="Daftar"/>
-        &nbsp;
-        <input type="reset" value="Batal"/>
-    </form>
+    <div class="container-fluid">
+        <div class="mt-1 p-3 bg-primary text-white rounded">
+            <h1 style="text-align: center">Form Pendaftaran UKM</h1>
+        </div>
+        <div class="mt-2 card">
+        <form action="proses_daftar.php" method="POST">
+            <div class="card-body">
+                <div class="mb-3">
+                    <label for="var_npm" class="form-label">NPM:</label>
+                    <input type="text" class="form-control" placeholder="Input Nomor Pengenal Mahasiswa (NPM)" name="var_npm">
+                </div>
+                <div class="mb-3">
+                    <label for="var_nama" class="form-label">Nama:</label>
+                    <input type="text" class="form-control" placeholder="Input Nama Lengkap Mahasiswa" name="var_nama">
+                </div>
+                <div class="mb-3">
+                    <label for="var_ukm" class="form-label">Pilihan UKM:</label>
+                    <select name="var_ukm" class="form-select">
+                        <option>UKM Programming</option>
+                        <option>UKM Band</option>
+                        <option>UKM Futsal</option>
+                        <option>UKM Basket</option>
+                    </select>
+                </div>
+            </div>
+            <div class="card-footer">
+            <button type="submit" class="btn btn-dark">Daftar</button>
+            <button type="reset" class="btn btn-danger">Batal</button>
+            </div>
+        </form>
+        </div>
+    </div>
 </body>
 </html>
--- proses_daftar.php
+++ proses_daftar.php
@@ -1,15 +1,49 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proses Daftar</title>
+    <!-- Latest compiled and minified CSS -->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <?php
     //Disini kita tangkap 3 variabel yang dikirim
     //dari halaman index.php
     $tangkap_npm = $_POST["var_npm"];
     $tangkap_nama = $_POST["var_nama"];
     $tangkap_ukm = $_POST["var_ukm"];
-
-    //tampilin yang udah ditangkap pake echo atau print
-    echo $tangkap_npm;
-    echo "<br/>";
-    echo $tangkap_nama;
-    echo "<br/>";
-    echo $tangkap_ukm;
-    echo "<br/>";
     ?>
+    <div class="container-fluid">
+        <div class="mt-1 p-3 bg-primary text-white rounded">
+            <h1 style="text-align: center">Informasi Pendaftaran</h1>
+        </div>
+        <div class="mt-2 card">
+            <div class="card-body">
+                <div class="mb-3">
+                    <label for="var_npm" class="form-label"><strong>Nomor Pengenal Mahasiswa (NPM):</strong></label>
+                    <?php
+                    echo $tangkap_npm;?>
+                </div>
+                <div class="mb-3">
+                    <label for="var_nama" class="form-label"><strong>Nama Lengkap Mahasiswa:</strong></label>
+                    <?php
+                    echo $tangkap_nama;?>
+                </div>
+                <div class="mb-3">
+                    <label for="var_npm" class="form-label"><strong>Unit Kegiatan Mahasiswa (UKM) yang Diikuti:</strong></label>
+                    <?php
+                    echo $tangkap_ukm;?>
+                </div>
+            </div>
+            <div class="card-footer">
+                <form action="index.php" method="Post">
+                <button type="Selesai" class="btn btn-success">Selesai</button>
+            </form>
+            </div>
+        
+</body>
+</html>
