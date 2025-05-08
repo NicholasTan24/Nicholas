15633325a2585fb15c749159a98dc3c9bbc543b9
NicholasTan24 <nicholastan_2327240005@mhs.mdp.ac.id>
2025-05-08 10:50:37 +0700

    connect database dengan html

--- pendaftaran_ukm/koneksi.php
+++ pendaftaran_ukm/koneksi.php
@@ -5,3 +5,8 @@ $pass = "";
 $dbname = "db_ukm";
 
 $connect = mysqli_connect($host,$user,$pass,$dbname) or die('Database Tidak Terhubung');
+// Check connection
+if ($connect -> connect_errno) {
+    echo "Failed to connect to MySQL: " . $connect -> connect_error;
+    exit();
+  }
